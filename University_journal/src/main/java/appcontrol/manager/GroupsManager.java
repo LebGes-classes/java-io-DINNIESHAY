b61366cb64696d5d1de4778b14dc4eb66676813c
DINNIESHAY <din200510@gmail.com>
2025-05-17 10:52:18 +0300

    Добавлено управление группами

--- University_journal/src/main/java/appcontrol/manager/GroupsManager.java
+++ University_journal/src/main/java/appcontrol/manager/GroupsManager.java
@@ -1,12 +1,30 @@
 package appcontrol.manager;
 
+import appcontrol.visual.services.Services;
+import database.access.GroupsAccess;
+import university.group.Group;
+
+import java.util.ArrayList;
+
 public class GroupsManager {
 
     public static void addGroup() {
-
+        System.out.println("Enter group's name:");
+        String groupName = Services.getInput();
+        Group group = new Group(groupName);
+        GroupsAccess.add(group);
     }
 
     public static void printInfo() {
-
+        ArrayList<Group> groups = GroupsAccess.getAll();
+        if (groups.isEmpty()) {
+            System.out.println("No groups");
+        } else {
+            for (Group group : groups) {
+                System.out.print(group);
+            }
+        }
+        System.out.println("\nPress any key to go back");
+        Services.getInput();
     }
 }
